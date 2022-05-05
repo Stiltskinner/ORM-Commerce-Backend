77ef6fe3d2f48b8d982b4fcdad1375e2fe6df71c
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-04 20:19:09 -0500

    Got category post route working

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -34,15 +34,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// create new product
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+// Create a new product
 router.post('/', async (req, res) => {
 
   Product.create(req.body)
