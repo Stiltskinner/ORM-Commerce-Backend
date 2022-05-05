0cd9c2f7355b97f421722af7cf58b755048c10d1
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-04 21:22:39 -0500

    tag put route working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -38,10 +38,23 @@ router.get('/:id', async (req, res) => {
     } catch (err) {
       res.status(400).json(err);
     }
-  });
+});
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
